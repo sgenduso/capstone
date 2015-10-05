c30dc2259f7bbb2242faed0eab5480bdde59063c
Sam Genduso <sam.genduso@gmail.com>
2015-10-05 14:16:13 -0600

    board populates as table with x and y coordinates

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,23 +1,18 @@
 app.controller('player1Controller', ['$scope', function ($scope) {
-  $scope.rows=[1,2,3,4,5,6,7,8,9,10];
-  $scope.myData = [
-    {
-        "firstName": "Cox",
-        "lastName": "Carney",
-        "company": "Enormo",
-        "employed": true
-    },
-    {
-        "firstName": "Lorraine",
-        "lastName": "Wise",
-        "company": "Comveyer",
-        "employed": false
-    },
-    {
-        "firstName": "Nancy",
-        "lastName": "Waters",
-        "company": "Fuelton",
-        "employed": false
-    }
-];
+  $scope.gridSize=function (size) {
+    return new Array(size);
+  };
+//   $scope.gridOptions = {
+//     enableSorting: false,
+//     showHeader: false,
+//     data: [
+//     {
+//         pos: "Cox",
+//         "lastName": "Carney",
+//         "company": "Enormo",
+//         "employed": true
+//     },
+//
+// ]
+// };
 }]);
