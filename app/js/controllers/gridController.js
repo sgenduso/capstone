8b22ba7f2e54cb98a3b5f2562fa9b8f177aadbb9
Sam Genduso <sam.genduso@gmail.com>
2015-10-15 10:08:08 -0600

    ships rotate both ways YAY

--- app/js/controllers/gridController.js
+++ app/js/controllers/gridController.js
@@ -68,80 +68,60 @@ app.controller('gridController', ['$scope', 'gameService', '$firebaseObject', fu
 $scope.previousCells = gameService.previousCells;
 
 $scope.rotateToVert = function (e) {
-  var startDirection = e.data.startDirection;
+  console.log('rotating to vertical');
   var dropCells = e.data.dropCells;
+  var ship = e.data.ship;
+  var size = e.data.size;
   var count = 0;
   var rotatedCells = [];
-  // $.each(dropCells, function (index, cell) {
-  //   // console.log(count);
-  //   if (index > 0) {
-  //     if (startDirection === 'horizontal') {
-  //       rotatedCells.push($('td[data-x=' + (Number($(this).attr('data-x'))-count) + '][data-y=' + (Number($(this).attr('data-y'))+count) + ']')[0]);
-  //     } else{
-  //       rotatedCells.push($('td[data-x=' + (Number($(this).attr('data-x'))+count) + '][data-y=' + (Number(e.attr('data-y'))-count) + ']')[0]);
-  //     }
-  //   }
-  //   count++;
-  // });
-  // console.log(rotatedCells);
-  console.log(dropCells);
+
   $.each(dropCells, function (index, cell) {
-    // console.log(cell);
-    // console.log(cell.dataset.x);
-    // console.log(cell.dataset.y);
-    // console.log(Number(cell.dataset.x)-count);
-    // console.log(Number(cell.dataset.y)+count);
-    // if (startDirection === 'horizontal') {
     count ++;
-    // console.log($('td[data-x=' + (Number(cell.dataset.x)-count) + ']')[0]);
-    console.log('rotated cell : ');
-    console.log($('td[data-x=' + (Number(cell.dataset.x)-count) + '][data-y=' + (Number(cell.dataset.y)+count) + ']')[0]);
     if (index > 0) {
       rotatedCells.push($('td[data-x=' + (Number(cell.dataset.x)- count + 1) + '][data-y=' + (Number(cell.dataset.y) + count - 1) + ']')[0]);
     }
-    // } else {
-    //   return $('td[data-x=' + (Number(val.dataset.x)+count) + '][data-y=' + (Number(val.dataset.y)-count) + ']')[0];
-    // }
   });
-  console.log('rotated cells: ');
-    console.log(rotatedCells);
-  if(gameService.allSpacesFree(rotatedCells) && gameService.roomOnBoard(rotatedCells.length+1, e.data.size)){
-    console.log('in rotate function');
-    console.log(rotatedCells);
-  // if (startDirection === 'horizontal') {
-    // var count = 1;
+  if(gameService.allSpacesFree(rotatedCells) && gameService.roomOnBoard(rotatedCells.length+1, size)){
     $.each(dropCells, function (index, cell) {
       if (index > 0) {
-      // var swappedCell = $('td[data-x=' + (Number($(this).attr('data-x'))-count) + '][data-y=' + (Number($(this).attr('data-y'))+count) + ']');
-      // console.log(rotatedCells[index-1]);
-      // console.log(rotatedCells[index-1].attributes);
-      // console.log(rotatedCells[index-1].id);
-
-      $scope.p1Board[(rotatedCells[index-1]).id].boat = e.data.ship;
-      $scope.p1Board[$(this).attr('id')].boat = false;
-      $scope.p1Board.$save();
-      // count++;
+        $scope.p1Board[(rotatedCells[index-1]).id].boat = ship;
+        $scope.p1Board[$(this).attr('id')].boat = false;
+        $scope.p1Board.$save();
       }
     });
-    // e.data.startDirection = 'vertical';
-    // count = 1;
-  // } else {
-  //   // var count = 1;
-  //   $.each(dropCells, function (index, cell) {
-  //     if (index > 0) {
-  //     // var swappedCell = $('td[data-x=' + (Number($(this).attr('data-x'))+count) + '][data-y=' + (Number($(this).attr('data-y'))-count) + ']');
-  //     $scope.p1Board[(rotatedCells[index-1]).id].boat = e.data.ship;
-  //     $scope.p1Board[$(this).attr('id')].boat = false;
-  //     $scope.p1Board.$save();
-  //     // count++;
-  //     }
-  //   });
-  //   e.data.startDirection = 'horizontal';
-  //   count = 1;
-  // }
-  // console.log(e.data.startDirection);
-  // console.log(e.data.dropCells);
 }
+$(dropCells[0]).unbind('click');
+rotatedCells.unshift(dropCells[0]);
+$(dropCells[0]).click({dropCells: rotatedCells, ship: ship, size: size}, $scope.rotateToHor);
+
+};
+
+$scope.rotateToHor = function (e) {
+  console.log('rotating to horizontal');
+  var dropCells = e.data.dropCells;
+  var ship = e.data.ship;
+  var size = e.data.size;
+  var count = 0;
+  var rotatedCells = [];
+
+  $.each(dropCells, function (index, cell) {
+    count ++;
+    if (index > 0) {
+      rotatedCells.push($('td[data-x=' + (Number(cell.dataset.x)+ count - 1) + '][data-y=' + (Number(cell.dataset.y) - count + 1) + ']')[0]);
+    }
+  });
+  if(gameService.allSpacesFree(rotatedCells) && gameService.roomOnBoard(rotatedCells.length+1, size)){
+    $.each(dropCells, function (index, cell) {
+      if (index > 0) {
+        $scope.p1Board[(rotatedCells[index-1]).id].boat = ship;
+        $scope.p1Board[$(this).attr('id')].boat = false;
+        $scope.p1Board.$save();
+      }
+    });
+}
+$(dropCells[0]).unbind('click');
+rotatedCells.unshift(dropCells[0]);
+$(dropCells[0]).click({dropCells: rotatedCells, ship: ship, size: size}, $scope.rotateToVert);
 };
 
 $scope.dropped = function(dragEl, dropEls) {
@@ -160,7 +140,7 @@ $scope.dropped = function(dragEl, dropEls) {
 
 
       if(gameService.allSpacesFree(drop) && !gameService.shipOnBoard(drag.ship) && gameService.roomOnBoard(drop.length, drag.size)){
-        $(drop[0]).click({dropCells: drop, ship: drag.ship, size: drag.size, startDirection: 'horizontal'}, $scope.rotateToVert);
+        $(drop[0]).click({dropCells: drop, ship: drag.ship, size: drag.size}, $scope.rotateToVert);
         $.each(drop,function (index, cell) {
           $scope.p1Board[$(this).attr('id')].boat = $scope.p1Board[$(this).attr('id')].boat || drag.ship;
           $scope.shipsOnBoard[drag.ship] = true;
